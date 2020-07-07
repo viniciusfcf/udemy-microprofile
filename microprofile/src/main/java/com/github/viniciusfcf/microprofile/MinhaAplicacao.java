8c41286a1f3eb6821bdf80443f7fd9dbdddbea4f
Vinicius <vinicius.ferraz@gmail.com>
2020-07-07 01:21:29 -0300

    Cria Projeto Inicial

---
+++ microprofile/src/main/java/com/github/viniciusfcf/microprofile/MinhaAplicacao.java
@@ -0,0 +1,9 @@
+package com.github.viniciusfcf.microprofile;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/")
+public class MinhaAplicacao extends Application{
+    
+}
