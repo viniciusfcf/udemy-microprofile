e63fef3b2b68c6730e6539c6f77ba6ec4a1c5ed1
Vinicius <vinicius.ferraz@gmail.com>
2020-07-09 01:39:50 -0300

    propagando headers

--- microprofile/src/main/java/com/github/viniciusfcf/microprofile/restclient/MPRestClientResource.java
+++ microprofile/src/main/java/com/github/viniciusfcf/microprofile/restclient/MPRestClientResource.java
@@ -1,10 +1,16 @@
 package com.github.viniciusfcf.microprofile.restclient;
 
+import java.net.URI;
+import java.net.URISyntaxException;
+
 import javax.enterprise.context.ApplicationScoped;
+import javax.enterprise.inject.spi.CDI;
 import javax.inject.Inject;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 
+import org.eclipse.microprofile.rest.client.RestClientBuilder;
+import org.eclipse.microprofile.rest.client.RestClientDefinitionException;
 import org.eclipse.microprofile.rest.client.inject.RestClient;
 
 
@@ -20,7 +26,28 @@ public class MPRestClientResource {
     public String adicionarComGet() {
         Integer idUF  = 222;
         MunicipioDTO municipio = new MunicipioDTO(null, "Maceio", 12000);
-		return service.adicionar(idUF, municipio).toString();
+		return service.adicionar("Valor1", idUF, municipio).toString();
+    }
+
+
+    @GET
+    @Path("sem-cdi")
+    public String adicionarComGet2() {
+        Integer idUF  = 222;
+        MunicipioDTO municipio = new MunicipioDTO(null, "Maceio", 12000);
+        MunicipioService serviceInterno = CDI.current()
+            .select(MunicipioService.class, RestClient.LITERAL).get();
+		return serviceInterno.adicionar("Valor2", idUF, municipio).toString();
+    }
+
+    @GET
+    @Path("builder")
+    public String adicionarComGet3() throws IllegalStateException, RestClientDefinitionException, URISyntaxException {
+        Integer idUF  = 222;
+        MunicipioDTO municipio = new MunicipioDTO(null, "Maceio", 12000);
+        MunicipioService serviceInterno = RestClientBuilder.newBuilder()
+            .baseUri(new URI("http://localhost:8080")).build(MunicipioService.class);
+		return serviceInterno.adicionar("Valor3", idUF, municipio).toString();
     }
 
 }
--- microprofile/src/main/java/com/github/viniciusfcf/microprofile/restclient/MunicipioService.java
+++ microprofile/src/main/java/com/github/viniciusfcf/microprofile/restclient/MunicipioService.java
@@ -1,19 +1,42 @@
 package com.github.viniciusfcf.microprofile.restclient;
 
-import javax.ws.rs.GET;
+import javax.ws.rs.HeaderParam;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.QueryParam;
 
+import org.eclipse.microprofile.rest.client.annotation.ClientHeaderParam;
+import org.eclipse.microprofile.rest.client.annotation.RegisterClientHeaders;
+import org.eclipse.microprofile.rest.client.ext.DefaultClientHeadersFactoryImpl;
 import org.eclipse.microprofile.rest.client.inject.RegisterRestClient;
 
 @Path("municipios")
 @RegisterRestClient(configKey = "correios")
+@ClientHeaderParam(name = "UtilizandoClientHeaderParamInterface", value = "X1")
+@RegisterClientHeaders(DefaultClientHeadersFactoryImpl.class)
 public interface MunicipioService {
     
     @POST
-    @GET
-    public MunicipioDTO adicionar(@QueryParam("idUF")Integer idUF, 
+    @ClientHeaderParam(name = "UtilizandoClientHeaderParamMetodo", value = "X2")
+    @ClientHeaderParam(name = "TokenGerado", value = "{gerarToken}")
+    public MunicipioDTO adicionar(
+        @HeaderParam("UtilizandoHeaderParam")
+        String header,
+        @QueryParam("idUF")Integer idUF, 
         MunicipioDTO municipio);
 
+    default String gerarToken(String header) {
+        return header + "123";
+    }
+
+
+    class BeanParamExemplo {
+
+        @HeaderParam("UtilizandoHeaderParam")
+        String header;
+
+        @QueryParam("idUF")Integer idUF;
+
+        MunicipioDTO municipio;
+    }
 }
